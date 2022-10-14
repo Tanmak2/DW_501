76448b9a648a0b3f4c7a830a2939420c17650f67
Tanmak2 <whdcks3131@naver.com>
2022-10-14 14:15:39 +0900

    feat : 네트워크 소켓 공부(1:1대화가능)

--- python/server.py
+++ python/server.py
@@ -1,18 +1,34 @@
 from socket import *
 import threading
 import time
+from tkinter import *
+
+root = Tk()
+root.title('대화방')
+root.geometry("300x300+100+100")
+scroll = Scrollbar(root)
+scroll.pack(side="right")
+list = Listbox(root, width=200, height=280)
+list.pack()
 
 
 def send(sock):
     while True:
         sendData = input('>>>')
         sock.send(sendData.encode('utf-8'))
+        if sendData != "":            
+            list.insert(END, '나 : '+sendData)
+            list.see(END)
 
 
 def receive(sock):
     while True:
         recvData = sock.recv(1024)
-        print('상대방 :', recvData.decode('utf-8'))
+        list.insert(END, '상대방 : '+recvData.decode('utf-8'))
+        list.see(END)
+            
+            
+            
 
 
 port = 9080
@@ -32,6 +48,7 @@ receiver = threading.Thread(target=receive, args=(connectionSock,))
 
 sender.start()
 receiver.start()
+root.mainloop()
 
 while True:
     time.sleep(1)
