ce31a86e68c1a654ee1eab26a71ecb6fac0f8128
Tanmak2 <whdcks3131@naver.com>
2022-10-14 14:14:26 +0900

    feat : 1014 오전수업

--- html/오전수업/javascript/1013/1013_2.js
+++ html/오전수업/javascript/1013/1013_2.js
@@ -1,3 +1,18 @@
+
+function movie(title, dir, year, type){
+    this.movie_title = title;
+    this.movie_dir = dir;
+    this.movie_year = year;
+    this.movie_type = type;
+}
+
+movie.prototype.out = function(){
+    return this.movie_title+" "+this.movie_dir+" "+this.movie_year+" "+this.movie_type;
+}
+
+var movie_list = new movie();
+
+
 window.onload = function (){
     var input = document.getElementsByClassName("movie");
     for(var i=0; i<input.length; i++){
@@ -5,7 +20,25 @@ window.onload = function (){
             this.setAttribute("class", "movie active");
         });
         input[i].addEventListener("blur", function(){
-            this.setAttribute("class", "movie");
+            if(this.value == 0){
+                this.setAttribute("class", "movie");
+            }            
         });
     }
+}
+
+function enroll(){
+    var val_temp = document.getElementsByClassName("movie");
+
+    var data = new movie(val_temp[0].value,val_temp[1].value,val_temp[2].value,val_temp[3].value);
+
+    movie_list.push(data);
+    print();
+}
+
+function print(){
+    var li = document.createElement("li");
+    li.innerText = movie_list[movie_list.length-1].out();
+
+    document.getElementById("movie_list").append(li);
 }
